254ed9cb31ea7c783bc27146514ad2b97ecb4232
Henrique <henrique.ajnbranco@gmail.com>
2022-04-03 19:43:58 -0300

    upload: material da aula 13, quadrado magico 4x4

---
+++ Aula13--05-04-2022-projetos-portifolio-quad-mag/quadrado_magico_4x4.py
@@ -0,0 +1,20 @@
+"""
+How Bootcamps - Stone - /código[s]
+Autor: Henrique Junqueira Branco
+
+Enunciado:
+Quadrado mágico: Um quadrado mágico é aquele dividido em linhas e colunas, 
+com um número em cada posição e no qual a soma das linhas, colunas e diagonais é a mesma. 
+Por exemplo, veja um quadrado mágico de lado 4, com números de 1 a 16:
+01  05  09  16
+06  07  02  10
+08  03  04  11
+12  15  14  13
+Elabore uma função que identifica e mostra na tela todos os quadrados mágicos com as características acima. 
+Dica: produza todas as combinações possíveis e verifique a soma quando completar cada quadrado. 
+Usar um vetor (lista) de 1 a 16 parece ser mais simples que usar uma matriz 4x4.
+
+Extra: Permita que o usuário indique o tamanho do cubo mágico (2x2, 3x3, 4x4, etc.)
+"""
+
+# Sem dicas agora! Tentem elaborar toda a lógica do zero!
